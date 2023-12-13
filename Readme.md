991ace95c3de029d560ebe1e45e81fac71f39cd9
nitaysason <nitaysason@gmail.com>
2023-12-13 09:45:52 +0200

    first commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# This is my first project in py
+
+## Action
+    Add - Done
+    Print - Done
+    Exit - Done
+    Delete
+    Edit - Done
+    Save data to file-Done 
+    Load data from a file- Done
---
+++ app.py
@@ -0,0 +1,85 @@
+import json
+import os
+
+
+Garage = []
+MY_DATA = 'output.txt'
+
+   
+def load_data():
+    global Garage
+    with open(MY_DATA, 'r') as filehandle:
+        Garage = json.load(filehandle) 
+
+def save_2_file():
+    with open(MY_DATA, 'w') as filehandle:
+        json.dump(Garage, filehandle)
+
+
+def do_menu_actions(userSelection):
+    if userSelection == 'x': # when closing the applicaion
+        save_2_file()
+        print("bye bye")
+        exit()
+    elif userSelection == 'p': print(Garage)
+    elif userSelection == 'a': Garage.append({"color":input("which color?"),"model":input("which model?"),"brand":input("which brand?")})
+    elif userSelection == 'e':   
+ 
+        print("Edit existing car:")
+        print(Garage)  # רשימת המכוניות בג'ראג'
+
+        car_index = int(input("Enter the index of the car you want to edit: "))
+        if 0 <= car_index < len(Garage):
+        # מדפיס את פרטי המכונית הנוכחית
+            print(f"Current details: {Garage[car_index]}")
+
+        # שינוי פרטי המכונית
+            Garage[car_index]["color"] = input("Enter new color: ")
+            Garage[car_index]["model"] = input("Enter new model: ")
+            Garage[car_index]["brand"] = input("Enter new brand: ")
+
+            print(f"Car at index {car_index} has been updated.")
+            print(Garage)  # רשימת המכוניות לאחר העדכון
+        else:
+            print("Invalid index. No car has been edited.")
+    elif userSelection == 'd': 
+     
+
+        print("Delete existing car:")
+        print(Garage)  # רשימת המכוניות בג'ראג'
+
+        car_index = int(input("Enter the index of the car you want to delete: "))
+        if 0 <= car_index < len(Garage):
+        # מדפיס את פרטי המכונית שימומשת
+            print(f"Deleting car: {Garage[car_index]}")
+
+        # מחיקת המכונית מרשימת המכוניות
+            deleted_car = Garage.pop(car_index)
+
+            print(f"Car at index {car_index} has been deleted: {deleted_car}")
+            print(Garage)  # רשימת המכוניות לאחר המחיקה
+        else:
+            print("Invalid index. No car has been deleted.")
+
+
+    
+
+def menu():
+    print("p - print all cars")
+    print("x - exit")
+    print("d - delete")
+    print("a - add a car")
+    print("e - edit the car")
+
+
+def main():
+    load_data() 
+    while True:
+        menu()
+        userSelection = input("please select action")
+        os.system('cls' if os.name == 'nt' else 'clear') #clear terminal
+        do_menu_actions(userSelection)
+        
+
+if __name__ == "__main__":
+    main()
